a063cdc054bd8c75d01175c133f22f2c14b83983
brahimaann <brahimaann3@gmail.com>
2023-05-06 20:51:26 +0100

    Initial commit

---
+++ src/components/Team.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export const Team = (props) => {
+  return (
+    <div id="team" className="text-center">
+      <div className="container">
+        <img src="img/Chairman.jpg" className="img-responsive" alt="" style={{ margin: "0 auto" }}/>
+        <div className="col-md-8 col-md-offset-2 section-title">
+          <h2>Founder and Chairman</h2>
+          <p>
+          Dr. Brahima D. Kaba is an Educator with over 40 years of teaching, research and administrative experience both locally and internationally. He was the former Chairman, Sociology Department, the University of Liberia, where he taught Sociology, Political Science, and French, amongst other things. He also taught as an External Faculty at the W. Dubois Afro-African Studies Center, Harvard University, Cambridge, MA, USA and an Adjunct Professor, Social Sciences, University of Maryland University College. As a public servant, he served as the Minister of Commerce and Industry as well as the Minister of Transport of the Republic of Liberia, after which he went into the diplomatic service. He served as Ambassador of Liberia to the Arab Republic of Egypt, Tunisia, Jordan, Sudan, Saudi Arabia, the Kingdom of Saudi Arabia, The Kingdom of Bahrain, and the Sultanate of Oman. Senegal, Mauritania, the Gambia and Cabo Verde with oversight over the UAE, Kuwait.
+          </p>
+
+        </div>
+       His publications include "The Role of Education in Combating Violence," "Christianity, Islam and African Religions in Liberia"; "Relationships between Women Household Roles and their Outside Activities"; "Modernization in Liberia (1847-1970)." The author is currently working on his memoir and several other projects.
+
+      </div>
+    </div>
+  );
+};
